fd6d70f00c32cc3d64545b67dde460f994b544e0
Rohanjha121 <rohanjha1407@gmail.com>
2026-06-19 09:18:59 +0530

    Fixed Inngest and Clerk Middleware

--- server/server.js
+++ server/server.js
@@ -9,12 +9,12 @@ const app = express();
 
 app.use(express.json());
 app.use(cors())
+app.use(clerkMiddleware());
 
 app.get('/', (req, res)=> res.send('Server is live!'));
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
-// app.use(clerkMiddleware());
 
 
 const PORT = process.env.PORT || 5000
